f0c01e85a8d575997a68160e6c460f005ced07a6
Dennis Reimann <mail@dennisreimann.de>
2025-01-02 09:47:36 +0100

    Cache and limit

--- helpers.mjs
+++ helpers.mjs
@@ -3,28 +3,35 @@ import { join } from 'path'
 import { run } from "@mermaid-js/mermaid-cli"
 
 const { MEMPOOL_SPACE_BASE_URL } = process.env
-const MP_CACHE = {}
 
 const mmdConfig = { backgroundColor: 'transparent', htmlLabels: false, puppeteerConfig: { headless: 'new' } }
 
 const getMempoolUrl = path => `${MEMPOOL_SPACE_BASE_URL || 'https://mempool.space'}/${path}`
 
 const queryMempoolSpace = async apiPath => {
-  if (typeof MP_CACHE[apiPath] == 'undefined') {
-    const url = getMempoolUrl(`api/${apiPath}`)
-    try {
-      const res = await fetch(url)
-      MP_CACHE[apiPath] = res.ok ? await res.json() : null
-    } catch (e) {
-      console.error('Error querying Mempool Space at', url, e.message)
-      MP_CACHE[apiPath] = null
+  const url = getMempoolUrl(`api/${apiPath}`)
+  try {
+    const res = await fetch(url)
+    if (res.ok) {
+      const json = await res.json()
+      await mkdir(join('cache', 'tx'), { recursive: true })
+      await writeFile(join('cache', `${apiPath}.json`), JSON.stringify(json, null, 2))
+      return json
     }
+  } catch (e) {
+    console.error('Error querying Mempool Space at', url, e.message)
   }
-  return MP_CACHE[apiPath]
 }
 
 export const getMempoolSpaceData = async txId => {
-  const txData = await queryMempoolSpace(`tx/${txId}`)
+  const lookup = `tx/${txId}`;
+  let txData;
+  try {
+    const json = await import(`./cache/${lookup}.json`, { with: { type: 'json' } })
+    txData = json.default
+  } catch (e) {
+    txData = await queryMempoolSpace(lookup)
+  }
   if (!txData) return null
   const { vin: inputs, vout: outputs, status: { confirmed, block_height, block_time } } = txData
   return {
@@ -34,8 +41,8 @@ export const getMempoolSpaceData = async txId => {
   }
 }
 
-const slug = str => str.toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/^-+|-+$/g, '')
-const truncateCenter = (str, len = 7) => str.length <= len * 2 ? str : `${str.slice(0, len)}…${str.slice(-len)}`
+export const slug = str => str.toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/^-+|-+$/g, '')
+export const truncateCenter = (str, len = 7) => str.length <= len * 2 ? str : `${str.slice(0, len)}…${str.slice(-len)}`
 
 const toMermaid = (data, parent) => {
   let mermaid = ''
@@ -57,7 +64,7 @@ const toMermaid = (data, parent) => {
 }
 
 export const writeMermaidFile = async data => {
-  const title = data.title || `${data.Txid}:${data.Vout}`
+  const title = data.title || (data.Vout ? `${data.Txid}:${data.Vout}` : data.Txid)
   const mmd = `---
   title: "${title}"
   config:
@@ -68,7 +75,7 @@ export const writeMermaidFile = async data => {
     classDef txin stroke:#0f0`  + toMermaid(data)
   const _html = await readFile('./template.html', 'utf8')
   const html = _html.replace('#TITLE#', title).replace('#TMPL#',mmd).replace('#BASE#',MEMPOOL_SPACE_BASE_URL)
-  const name = title || `${data.Txid}_${data.Vout}`
+  const name = slug(title)
   await mkdir(join('generated', 'mmd'), { recursive: true })
   await mkdir(join('generated', 'svg'), { recursive: true })
   await mkdir(join('generated', 'html'), { recursive: true })
--- lookup.mjs
+++ lookup.mjs
@@ -1,18 +1,33 @@
 import { writeFile } from 'fs/promises'
 import { join } from 'path'
-import { getMempoolSpaceData, writeMermaidFile } from './helpers.mjs'
+import { getMempoolSpaceData, writeMermaidFile, slug } from './helpers.mjs'
 
 const outpoint = process.argv[2]
 const title = process.argv[3] || outpoint
+const lvl = process.argv[4] || 50
 
 const filterInputs = inputs => inputs.length > 1
   ? inputs.find(i => i.Value < 0)
   : inputs[0]
 
-const findInputs = (txs, input) =>
-  txs.find(tx => tx.Value + tx.Fee == input.Value)
+const findInputs = (txs, input) => {
+  let inp = txs.find(tx => tx.Value + (tx.Fee || 0) == input.Value)
+  // lookup with tolerance
+  if (!inp) inp = txs.find(tx => {
+    const val = tx.Value + (tx.Fee || 0)
+    return val + 1 == input.Value || val - 1 == input.Value
+  })
+  // fallback to simple transaction lookup
+  if (!inp) inp = filterInputs(txs)
+  return inp
+}
 
 const getData = async (txid, vout, index, level) => {
+  if (level > lvl) {
+    console.warn('max level reached')
+    return null
+  }
+
   const group = vout ? `${txid}:${vout}` : txid
   console.group(group)
 
@@ -36,15 +51,15 @@ const getData = async (txid, vout, index, level) => {
     console.groupEnd()
     return null
   }
-  console.log('Input from', entry.Wallet, '-', entry.Label || 'no label', mempoolData)
+  console.log('Input from', entry.Wallet, '-', entry.Label || 'no label')
 
   const v = vout ? parseInt(vout) : 0
   const data = { ...entry, Confirmed: mempoolData.Confirmed.Blockheight, Vout: v, Address: mempoolData.Outputs[v].Address }
 
   const inputs = []
   for (const input of mempoolData.Inputs) {
-    console.log(input)
     // check if we know about this input
+    if (!index[input.Txid]) continue
     const txInput = findInputs(index[input.Txid] || [], input)
     // if so, get their data from the index as well
     if (!txInput) continue
@@ -77,7 +92,7 @@ const getData = async (txid, vout, index, level) => {
 
   // write result to file
   data.title = title
-  const filename = title || (vout ? `${txId}_${vout}` : txId)
+  const filename = slug(title)
   await writeFile(join('generated', `${filename}.json`), JSON.stringify(data, null, 2))
 
   // mermaid
